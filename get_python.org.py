fc1e3e525b7e6d3876a1538aa57fc6452ebd3e77
velenk <38948738+velenk@users.noreply.github.com>
2018-06-28 22:54:13 +0800

    .

---
+++ get_python.org.py
@@ -0,0 +1,8 @@
+import urllib.request
+
+response = urllib.request.urlopen('http://www.python.org')
+
+#print(response.read().decode('utf-8'))
+print(response.status)
+print(response.getheaders())
+print(response.getheader('Server'))
