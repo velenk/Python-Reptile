a58393b69fe5bc5b99e4f9ecb6e171fbcb55322e
velenk <38948738+velenk@users.noreply.github.com>
2018-07-02 13:25:27 +0800

    not_found

--- timeout.py
+++ timeout.py
@@ -3,7 +3,8 @@ import urllib.error
 import socket
 
 try:
-    response = urllib.request.urlopen('http://httpbin.org/get', timeout = 0.1)
+    response = urllib.request.urlopen('http://httpbin.org/get', timeout = 0.01)
 except urllib.error.URLError as e:
+    print(type(e.reason))
     if isinstance(e.reason, socket.timeout):
         print('Time Out')
