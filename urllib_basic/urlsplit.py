33ae716e78b71deb66174420d21c34d7814211a1
velenk <38948738+velenk@users.noreply.github.com>
2018-07-02 22:02:54 +0800

    urlunsplit

---
+++ urllib_basic/urlsplit.py
@@ -0,0 +1,5 @@
+from urllib.parse import urlsplit 
+
+result = urlsplit('http://www.baidu.com/index.html;user?id=5#comment')
+print(result)
+print(result.scheme, result[1])
--- urllib_basic/urlunprase.py
+++ urllib_basic/urlunprase.py
@@ -1,4 +1,4 @@
 from urllib.parse import urlunparse
 
-data = ['http', 'www.baidu.com', 'index.com', 'user', 'a=6', 'comment']
+data = ['http', 'www.baidu.com', 'index.html', 'user', 'a=6', 'comment']
 print(urlunparse(data))
